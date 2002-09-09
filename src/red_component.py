7f80fcfaef7cd7ea609afbbd7946b8eeb2d3e978
Jon Trowbridge <trow@src.gnome.org>
2002-09-09 20:14:19 +0000

    Forgot to add this earlier.
    svn path=/trunk/; revision=25

---
+++ src/red_component.py
@@ -0,0 +1,88 @@
+###
+### Copyright 2002 Ximian, Inc.
+###
+### This program is free software; you can redistribute it and/or modify
+### it under the terms of the GNU General Public License, version 2,
+### as published by the Free Software Foundation.
+###
+### This program is distributed in the hope that it will be useful,
+### but WITHOUT ANY WARRANTY; without even the implied warranty of
+### MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+### GNU General Public License for more details.
+###
+### You should have received a copy of the GNU General Public License
+### along with this program; if not, write to the Free Software
+### Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307, USA.
+###
+
+import gobject, gtk
+
+valid_widget_types = ["upper", "lower", "main"]
+
+class Component(gobject.GObject):
+
+    def __init__(self):
+        gobject.GObject.__init__(self)
+
+        self.have_server = 0
+        self.server_obj = None
+
+        self.widgets = {}
+        self.did_build = 0
+        self.visible_flag = 0
+
+    def get(self, type):
+        assert type in valid_widget_types
+        return self.widgets.get(type)
+
+    def display(self, type, widget):
+        assert type in valid_widget_types
+        if id(self.widgets.get(type)) != id(widget):
+            self.widgets[type] = widget
+            self.emit("display", type, widget)
+
+    def visible(self, flag):
+        if flag and not self.did_build:
+            self.build()
+            self.did_build = 1
+        if not self.visible_flag ^ flag:
+            self.visible_flag = flag
+            self.emit("visible", flag)
+
+    def is_visible(self):
+        return self.visible_flag
+
+    def server(self):
+        assert self.have_server
+        return self.server_obj
+
+    def set_server(self, server):
+        assert not self.have_server
+        self.server_obj = server
+        self.have_server = 1
+
+    def name(self):
+        return "?Unknown?"
+
+    def long_name(self):
+        return self.name()
+
+    def pixbuf(self):
+        return None
+
+    def build(self):
+        pass
+
+gobject.type_register(Component)
+
+gobject.signal_new("display",
+                   Component,
+                   gobject.SIGNAL_RUN_LAST,
+                   gobject.TYPE_NONE,
+                   (gobject.TYPE_STRING, gtk.Widget.__gtype__))
+
+gobject.signal_new("visible",
+                   Component,
+                   gobject.SIGNAL_RUN_LAST,
+                   gobject.TYPE_NONE,
+                   (gobject.TYPE_BOOLEAN,))
